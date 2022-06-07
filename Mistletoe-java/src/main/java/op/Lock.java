c78a138fb22accdf7f412fbde0d9a5f6da4401e4
K1nusan <lzy9986@126.com>
2022-06-07 17:51:14 +0800

    lock

--- Mistletoe-java/src/main/java/op/Lock.java
+++ Mistletoe-java/src/main/java/op/Lock.java
@@ -32,4 +32,31 @@ public class Lock {
         }
         return null;
     }
+    public static CompilationUnit addLock(CompilationUnit cu,String random){
+        ReentrantLock lock=new ReentrantLock();
+        try {
+            ClassOrInterfaceDeclaration hw = cu.getClassByName("hw").get();
+
+            List<MethodDeclaration> x= hw.getMethods();
+            String st1 = "ReentrantLock lock=new ReentrantLock();";
+            String st2 = "lock.lock();" ;
+            String st3 = "lock.unlock();//解锁";
+            String st4 = "String ifdef111 = "+random+";";
+            String st5 = "String endif = "+random+";";
+            for(MethodDeclaration md:x){
+                System.out.println(md.getName());
+                md.getBody().get()
+                        .addStatement(0,StaticJavaParser.parseStatement(st5))
+                        .addStatement(0,StaticJavaParser.parseStatement(st3))
+                        .addStatement(0,StaticJavaParser.parseStatement(st2))
+                        .addStatement(0,StaticJavaParser.parseStatement(st1))
+                        .addStatement(0,StaticJavaParser.parseStatement(st4));
+                System.out.println(md);
+            }
+            return cu;
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return null;
+    }
 }
